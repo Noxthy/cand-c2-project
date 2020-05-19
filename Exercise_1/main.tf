0b326e8cec3a99457c1c49bcb2fb7d6d735d470e
Noxthy <devy@live.se>
2020-05-19 16:17:17 +0200

    Update main.tf

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,23 @@
 # TODO: Designate a cloud provider, region, and credentials
-
+provider "aws" {
+  access_key = "Hidden"
+  secret_key = "Hidden"
+  region = "us-east-1"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "Udacity_T2" {
+  count = "4"
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0990c531fec32238c"
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+
+resource "aws_instance" "Udacity_M4" {
+  count = "2"
+  ami = "ami-0323c3dd2da7fb37d"
+  instance_type = "m4.large"
+  subnet_id = "subnet-0990c531fec32238c"
+}
